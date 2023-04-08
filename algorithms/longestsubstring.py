08cefde073897a01cbca4d2ecf8b91fe78e65446
Samuel Massnick <smassnick24@adrian.edu>
2023-04-08 11:22:13 -0400

    second round of problems

---
+++ algorithms/longestsubstring.py
@@ -0,0 +1,23 @@
+# find the longest substring without repeating characters
+# given a string s, find the longest substring without repeating characters
+
+class Solution:
+    def lengthOfLongestSubstring(self, string):
+        """Works in most cases, not all"""
+        longest = ""
+        substring = ""
+        sub_list = []
+        for i in range(len(string)):
+            if string[i] in substring:
+                sub_list.append(substring)
+                substring = "" + string[i]
+            else:
+                substring += string[i]
+        for i in range(len(sub_list)):
+            if len(sub_list[i]) > len(longest):
+                longest = sub_list[i]
+        return len(longest)
+
+
+if __name__ == "__main__":
+    print(Solution().lengthOfLongestSubstring("aabcderfffghe"))
